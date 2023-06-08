a9613a786043794fa413f1187633eabb68ac1368
Anu-tochka <anuta-zh@mail.ru>
2023-06-08 23:41:15 +0300

    Add Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,42 +1,72 @@
-#include <stdio.h>
 #include <iostream>
+#include "Trie.h"
+
 using namespace std;
 
-void IsKPeriodic(const string str) {
-     int n = str.size();
-     char nc = str[n-1];
-     string patt;
-     int k=0;
-     int i=0;
-    // находим паттерн 
-     while  (str[i] != nc) {
-        patt[i] = str[i];
-        i++;
-        k++;
-        
-    }
-    //последний символ паттерна
-    if (str[i] == nc && str[i+1] == str[0]) {
-        patt[i] = str[i];
-        k++;
-        i++;
-    }
-    int d = patt.size();
-    bool err = 0;
-    //проверяем паттерны
-    while (i < n) {
-        for (int j=0; j<d; j++) {
-            if (patt[j] != str[i]) err=1; 
-            i++;
-        }
-        
-    }
-    if (!err) cout << "Pattern found at index " << k << endl;
-}
-
-int main()
+int main(char* ar[], int count)
 {
-    string str="abcabcabcabc";
-    IsKPeriodic(str);
+    Trie tr;
+
+    tr.add("ability");
+    tr.add("acacia");
+    tr.add("acorn");
+    tr.add("acumen");
+    tr.add("adder");
+	tr.add("ado");
+    tr.add("aerodrom");
+    tr.add("aeroplane");
+    tr.add("affray");
+    tr.add("age");
+    tr.add("aim");
+    tr.add("air");
+    tr.add("alder");
+    tr.add("ale");
+    tr.add("alloy");
+    tr.add("amber");
+    tr.add("amity");
+    tr.add("ancoir");
+    tr.add("angel");
+    tr.add("ant");
+    tr.add("apex");
+    tr.add("apple");
+    tr.add("apron");
+    tr.add("arm");
+    tr.add("army");
+    tr.add("arrow");
+    tr.add("ash");
+    tr.add("asp");
+    tr.add("atom");
+    tr.add("attic");
+    tr.add("auger");
+    tr.add("aunt");
+    tr.add("avarice");
+    tr.add("awe");
+    tr.add("axe");
+    tr.add("axis");
+    tr.add("azote");
+    
+	string s = '';
+    cout << "Введите начало слова на a: "; 
+    cin >> s;
+	string prefix(s);
+
+    Trie * current = Trie->search(prefix);
+
+    if (current == NULL or current == Trie->root) {
+        cout << "Нет подходящих слов" << endl;
+    } else {
+        bool haschildren = false;
+        for (int c = 0; c < SIZE; c++) {
+            if (current->children[c] != NULL) {
+                 haschildren = true; break;
+            }
+        }
+        if (haschildren == false) {
+            cout << "Нет подходящих слов" << endl;
+        } else {
+            cout << "Найдены слова: " << prefix << endl;
+            Trie->зrint(current, prefix);
+        }
+    }
     return 0;
 }
